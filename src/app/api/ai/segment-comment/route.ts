c85bd171e47e86156bf47310b95833411896663d
kjaerb <kjaerb@gmail.com>
2024-01-16 10:37:11 +0100

    runtime for route

--- src/app/api/ai/segment-comment/route.ts
+++ src/app/api/ai/segment-comment/route.ts
@@ -1,5 +1,3 @@
-export const runtime = "edge";
-
 import { Configuration, OpenAIApi } from "openai-edge";
 import { NextRequest, NextResponse } from "next/server";
 import { OpenAIStream, StreamingTextResponse } from "ai";
