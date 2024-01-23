207c4a09a7a868c7adc7a8320d38458c6448edb5
kjaerb <kjaerb@gmail.com>
2024-01-23 15:54:09 +0100

    feat: start downgrade

---
+++ src/lib/auth.ts
@@ -0,0 +1,22 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { db } from "@/server/db";
+import AzureADProvider from "next-auth/providers/azure-ad";
+import { NextAuthOptions } from "next-auth";
+
+const { AZURE_AD_CLIENT_SECRET, AZURE_AD_CLIENT_ID, AZURE_AD_TENANT_ID } =
+  process.env;
+
+export const authConfig = {
+  pages: {
+    signIn: "/auth",
+  },
+  providers: [
+    AzureADProvider({
+      clientId: AZURE_AD_CLIENT_ID,
+      clientSecret: AZURE_AD_CLIENT_SECRET,
+      tenantId: AZURE_AD_TENANT_ID,
+    }),
+  ],
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+} satisfies NextAuthOptions;
